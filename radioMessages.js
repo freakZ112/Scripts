1442ce72a8ed4960a1ac2799dd3b743a2f11c57e
freakZ112 <102925064+freakZ112@users.noreply.github.com>
2024-03-17 20:14:36 +0100

    Update radioMessages.js

--- radioMessages.js
+++ radioMessages.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         radioMessages
-// @version      1.0.3
+// @version      1.0.2
 // @author       freakZ112
 // @description  Sprechwünsche - da fliegen sie davon
 // @include      /^https?:\/\/(?:w{3}\.)?(?:leitstellenspiel\.de|(?:meldkamerspel|missionchief|missionchief-australia|nodsentralspillet|112-merkez|jogo-operador112|operador193|dyspetcher101-game|missionchief-japan|jocdispecerat112|missionchief-korea|hatakeskuspeli|dispecerske-centrum)\.com|missionchief\.co\.uk|centro-de-mando\.es|operatorratunkowy\.pl|larmcentralen-spelet\.se|operatore112\.it|operateur112\.fr|dispetcher112\.ru|alarmcentral-spil\.dk|operacni-stredisko\.cz|centro-de-mando\.mx)\/$/
