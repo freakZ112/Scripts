36b271baaa98e38e7c2516469502712110c74d36
freakZ112 <102925064+freakZ112@users.noreply.github.com>
2024-03-25 18:55:35 +0100

    Update Collector.js

--- Collector.js
+++ Collector.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Collector
-// @version      1.0.4
+// @version      1.0.5
 // @author       freakZ112
 // @description  Sammelt die Eier, Kürbis etc. ein
 // @include      *://leitstellenspiel.de/missions/*
@@ -12,7 +12,7 @@
 (function() {
 'use strict';
 
-$('a[href*="halloweenhunt"], a[href*="easteregg"], a[href*="valentinescollection", a[href*="claim_found_object_sync"]').click((e) => {
+$('a[href*="claim_found_object_sync"]').click((e) => {
 event.preventDefault();
 var target = $(event.currentTarget);
 $.ajax({url: target.attr("href")});
@@ -20,20 +20,6 @@ target.remove();
 })
 })();
 
-var easteregg = document.querySelectorAll('a[href*=easteregg]');
-if (easteregg.length == 1){
-    easteregg[0].click();
-}
-
-var halloweenhunt = document.querySelectorAll('a[href*=halloweenhunt]');
-if (halloweenhunt.length == 1){
-    halloweenhunt[0].click();
-}
-
-var valentinescollection = document.querySelectorAll('a[href*=valentinescollection]');
-if (valentinescollection.length == 1){
-   valentinescollection[0].click();
-}
 var claim_found_object_sync = document.querySelectorAll('a[href*=claim_found_object_sync]');
 if (claim_found_object_sync.length == 1){
    claim_found_object_sync[0].click();
